1cfa940aea40b899e7417de8d62cf7945a9d6061
JinhaHwang <jh.hwang@hanwha.com>
2023-01-16 17:09:40 +0900

    books-mesh 프로젝트 생성 및 books-service 프로젝트 에서 스웨거를 및 스웨거를 통해 oas 파일 생성

--- apps/books-service/src/main.ts
+++ apps/books-service/src/main.ts
@@ -3,8 +3,12 @@
  * This is only a minimal backend to get started.
  */
 
+import { writeFileSync } from 'node:fs';
+import {join} from "node:path";
+
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +16,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Books service example')
+    .setDescription('Everything about books')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  // /docs 로 스웨거 서비스
+  SwaggerModule.setup('docs', app, document);
+  // spec 파일 생성
+  writeFileSync(join(__dirname,'./books.json'), JSON.stringify(document, null, 2));
+
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
