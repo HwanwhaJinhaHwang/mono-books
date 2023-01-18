2fafa70fb6abcc085f5d4b2350b2c7b164a2d3fc
JinhaHwang <jh.hwang@hanwha.com>
2023-01-18 15:40:47 +0900

    chore: add msw init

--- apps/books/src/main.tsx
+++ apps/books/src/main.tsx
@@ -4,13 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+const prepare = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    import('./mocks/browser').then(({ worker }) => {
+      worker.start();
+    });
+  }
+};
+
+prepare().then(() => {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+});
